f98b5271e29cc8f8007f2e89a903c125928b8f15
Burragan <burakucarict@gmail.com>
2021-05-17 13:50:09 +0200

    Init

---
+++ src/main/java/io/swagger/model/User.java
@@ -0,0 +1,309 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.model.AccountWithTransactions;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * User
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-17T11:49:32.019Z[GMT]")
+
+
+public class User   {
+  @JsonProperty("id")
+  private Long id = null;
+
+  @JsonProperty("username")
+  private String username = null;
+
+  @JsonProperty("password")
+  private String password = null;
+
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("email")
+  private String email = null;
+
+  /**
+   * Gets or Sets role
+   */
+  public enum RoleEnum {
+    CUSTOMER("Customer"),
+    
+    EMPLOYEE("Employee");
+
+    private String value;
+
+    RoleEnum(String value) {
+      this.value = value;
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+      return String.valueOf(value);
+    }
+
+    @JsonCreator
+    public static RoleEnum fromValue(String text) {
+      for (RoleEnum b : RoleEnum.values()) {
+        if (String.valueOf(b.value).equals(text)) {
+          return b;
+        }
+      }
+      return null;
+    }
+  }
+  @JsonProperty("role")
+  private RoleEnum role = null;
+
+  @JsonProperty("account")
+  private AccountWithTransactions account = null;
+
+  @JsonProperty("dayLimit")
+  private Double dayLimit = null;
+
+  @JsonProperty("transactionLimit")
+  private Double transactionLimit = null;
+
+  public User id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   **/
+  @Schema(example = "1", description = "")
+  
+    public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public User username(String username) {
+    this.username = username;
+    return this;
+  }
+
+  /**
+   * Get username
+   * @return username
+   **/
+  @Schema(example = "BG12345", required = true, description = "")
+      @NotNull
+
+    public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public User password(String password) {
+    this.password = password;
+    return this;
+  }
+
+  /**
+   * Get password
+   * @return password
+   **/
+  @Schema(example = "hiIamapassword4$", required = true, description = "")
+      @NotNull
+
+    public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public User name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+   **/
+  @Schema(example = "Bert Geersen", required = true, description = "")
+      @NotNull
+
+    public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public User email(String email) {
+    this.email = email;
+    return this;
+  }
+
+  /**
+   * Get email
+   * @return email
+   **/
+  @Schema(example = "BertGeersen1@gmail.com", required = true, description = "")
+      @NotNull
+
+    public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public User role(RoleEnum role) {
+    this.role = role;
+    return this;
+  }
+
+  /**
+   * Get role
+   * @return role
+   **/
+  @Schema(example = "Customer", required = true, description = "")
+      @NotNull
+
+    public RoleEnum getRole() {
+    return role;
+  }
+
+  public void setRole(RoleEnum role) {
+    this.role = role;
+  }
+
+  public User account(AccountWithTransactions account) {
+    this.account = account;
+    return this;
+  }
+
+  /**
+   * Get account
+   * @return account
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public AccountWithTransactions getAccount() {
+    return account;
+  }
+
+  public void setAccount(AccountWithTransactions account) {
+    this.account = account;
+  }
+
+  public User dayLimit(Double dayLimit) {
+    this.dayLimit = dayLimit;
+    return this;
+  }
+
+  /**
+   * Get dayLimit
+   * @return dayLimit
+   **/
+  @Schema(description = "")
+  
+    public Double getDayLimit() {
+    return dayLimit;
+  }
+
+  public void setDayLimit(Double dayLimit) {
+    this.dayLimit = dayLimit;
+  }
+
+  public User transactionLimit(Double transactionLimit) {
+    this.transactionLimit = transactionLimit;
+    return this;
+  }
+
+  /**
+   * Get transactionLimit
+   * @return transactionLimit
+   **/
+  @Schema(description = "")
+  
+    public Double getTransactionLimit() {
+    return transactionLimit;
+  }
+
+  public void setTransactionLimit(Double transactionLimit) {
+    this.transactionLimit = transactionLimit;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    User user = (User) o;
+    return Objects.equals(this.id, user.id) &&
+        Objects.equals(this.username, user.username) &&
+        Objects.equals(this.password, user.password) &&
+        Objects.equals(this.name, user.name) &&
+        Objects.equals(this.email, user.email) &&
+        Objects.equals(this.role, user.role) &&
+        Objects.equals(this.account, user.account) &&
+        Objects.equals(this.dayLimit, user.dayLimit) &&
+        Objects.equals(this.transactionLimit, user.transactionLimit);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, username, password, name, email, role, account, dayLimit, transactionLimit);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class User {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    username: ").append(toIndentedString(username)).append("\n");
+    sb.append("    password: ").append(toIndentedString(password)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    email: ").append(toIndentedString(email)).append("\n");
+    sb.append("    role: ").append(toIndentedString(role)).append("\n");
+    sb.append("    account: ").append(toIndentedString(account)).append("\n");
+    sb.append("    dayLimit: ").append(toIndentedString(dayLimit)).append("\n");
+    sb.append("    transactionLimit: ").append(toIndentedString(transactionLimit)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ src/main/java/io/swagger/model/WithdrawalRequestBody.java
@@ -0,0 +1,105 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * WithdrawalRequestBody
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-17T11:49:32.019Z[GMT]")
+
+
+public class WithdrawalRequestBody   {
+  @JsonProperty("accountFrom")
+  private String accountFrom = null;
+
+  @JsonProperty("amount")
+  private Double amount = null;
+
+  public WithdrawalRequestBody accountFrom(String accountFrom) {
+    this.accountFrom = accountFrom;
+    return this;
+  }
+
+  /**
+   * Get accountFrom
+   * @return accountFrom
+   **/
+  @Schema(example = "NL55 RABO 1234 5678 90", required = true, description = "")
+      @NotNull
+
+    public String getAccountFrom() {
+    return accountFrom;
+  }
+
+  public void setAccountFrom(String accountFrom) {
+    this.accountFrom = accountFrom;
+  }
+
+  public WithdrawalRequestBody amount(Double amount) {
+    this.amount = amount;
+    return this;
+  }
+
+  /**
+   * Get amount
+   * @return amount
+   **/
+  @Schema(example = "20.23", required = true, description = "")
+      @NotNull
+
+    public Double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Double amount) {
+    this.amount = amount;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    WithdrawalRequestBody withdrawalRequestBody = (WithdrawalRequestBody) o;
+    return Objects.equals(this.accountFrom, withdrawalRequestBody.accountFrom) &&
+        Objects.equals(this.amount, withdrawalRequestBody.amount);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(accountFrom, amount);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class WithdrawalRequestBody {\n");
+    
+    sb.append("    accountFrom: ").append(toIndentedString(accountFrom)).append("\n");
+    sb.append("    amount: ").append(toIndentedString(amount)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
