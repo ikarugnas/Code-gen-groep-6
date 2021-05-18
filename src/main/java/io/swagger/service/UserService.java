74bc43ae169672c6af912b43e46886632ee52262
Stoopie2000 <brian.stoop@live.nl>
2021-05-18 14:39:12 +0200

    Added MVC
    Added mvc packages

---
+++ src/main/java/io/swagger/service/UserService.java
@@ -0,0 +1,21 @@
+package io.swagger.service;
+
+import io.swagger.model.RegisterDTO;
+import io.swagger.model.User;
+import io.swagger.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public UserService() {
+    }
+
+    public User createUser(RegisterDTO userDTO){
+        return userRepository.findUserByUsername(userDTO.getUsername());
+    }
+}
