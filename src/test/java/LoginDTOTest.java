ea01a92fffe608e519f52e4a85ea7ce7fc193833
Stoopie2000 <brian.stoop@live.nl>
2021-05-26 13:27:14 +0200

    Test with tests

---
+++ src/test/java/LoginDTOTest.java
@@ -0,0 +1,13 @@
+import io.swagger.model.LoginDTO;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class LoginDTOTest {
+
+    @Test
+    public void createLoginDTOShouldNotBeNull() {
+        LoginDTO loginDTO = new LoginDTO();
+        assertNotNull(loginDTO);
+    }
+}
---
+++ src/test/java/RegisterDTOTest.java
@@ -0,0 +1,14 @@
+import io.swagger.model.RegisterDTO;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class RegisterDTOTest {
+
+    @Test
+    public void createRegisterDTOShouldNotBeNull() {
+        RegisterDTO registerDTO = new RegisterDTO();
+        assertNotNull(registerDTO);
+    }
+
+}
